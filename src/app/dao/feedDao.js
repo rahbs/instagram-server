54370f679b20b949c0796df3f6f80bbb51f906bd
rahbs <rahbs@naver.com>
2020-11-13 15:25:55 +0900

    userfeed 게시물숫자 수정

--- src/app/dao/feedDao.js
+++ src/app/dao/feedDao.js
@@ -49,33 +49,34 @@ async function getUserInfo(userIdx){
     const connection = await pool.getConnection(async (conn) => conn);
     try{
         const getUserInfoQuery = `
-            SELECT profileImgUrl, user.userIdx as userIdx, user.userId as userId, user.name as userName,
-                    profileIntro, profileWebSite,followingNum, followerNum, feedNum
-            FROM
-                (SELECT profileImgUrl, userIdx, userId, name, profileIntro, profileWebSite
-                    FROM user) as user
-            JOIN
-                (SELECT user.userIdx, userId, COUNT(feed.userIdx) as feedNum
-                    FROM user
-                    LEFT JOIN feed
-                    ON user.userIdx = feed.userIdx
-                GROUP BY user.userIdx) as feedNum
-            ON user.userIdx = feedNum.userIdx
-            JOIN
-                (SELECT user.userIdx, COUNT(follow.followingUserIdx) as followingNum
-                    FROM user
-                    LEFT JOIN follow
-                    ON user.userIdx = follow.followingUserIdx
-                GROUP BY user.userIdx) as followingNum
-            ON user.userIdx = followingNum.userIdx
-            JOIN
-                (SELECT user.userIdx, COUNT(follow.followedUserIdx) as followerNum
-                    FROM user
-                    LEFT JOIN follow
-                    ON user.userIdx = follow.followedUserIdx
-                GROUP BY user.userIdx) as followedNum
-            ON user.userIdx = followedNum.userIdx
-            WHERE user.userIdx = ?;
+        SELECT profileImgUrl, user.userIdx as userIdx, user.userId as userId, user.name as userName,
+                profileIntro, profileWebSite,followingNum, followerNum, feedNum
+        FROM
+            (SELECT profileImgUrl, userIdx, userId, name, profileIntro, profileWebSite
+                FROM user) as user
+        JOIN
+            (SELECT user.userIdx, userId, COUNT(feed.userIdx) as feedNum
+                FROM user
+                LEFT JOIN feed
+                ON user.userIdx = feed.userIdx
+                WHERE feed.isDeleted = 'N'
+            GROUP BY user.userIdx) as feedNum
+        ON user.userIdx = feedNum.userIdx
+        JOIN
+            (SELECT user.userIdx, COUNT(follow.followingUserIdx) as followingNum
+                FROM user
+                LEFT JOIN follow
+                ON user.userIdx = follow.followingUserIdx
+            GROUP BY user.userIdx) as followingNum
+        ON user.userIdx = followingNum.userIdx
+        JOIN
+            (SELECT user.userIdx, COUNT(follow.followedUserIdx) as followerNum
+                FROM user
+                LEFT JOIN follow
+                ON user.userIdx = follow.followedUserIdx
+            GROUP BY user.userIdx) as followedNum
+        ON user.userIdx = followedNum.userIdx
+        WHERE user.userIdx = ?;
                     `;
 
         const getUserInfo = await connection.query(
